2342691836102678d91ceedeba1f4e67165ce278
Balázs Dukai <balazs.dukai@gmail.com>
2017-03-08 08:40:20 +0100

    NUTS 2010 levels

--- nuts_grouping.sql
+++ nuts_grouping.sql
@@ -1,29 +1,57 @@
-﻿create or replace view nuts_2006_levels as
+﻿drop view nuts_2006_levels;
+create or replace view nuts_2006_levels as
 with lvl0 as(
 select nuts_id, geom
-from nuts_2006_poly
+from nuts_2006_poly2
 where stat_levl_ = 0
-group by id
 ),
 lvl1 as (
 select nuts_id, geom
-from nuts_2006_poly
+from nuts_2006_poly2
 where stat_levl_ = 1
-group by id
 ),
 lvl2 as (
 select nuts_id, geom
-from nuts_2006_poly
+from nuts_2006_poly2
 where stat_levl_ = 2
-group by id
 ),
 lvl3 as (
 select nuts_id, geom
-from nuts_2006_poly
+from nuts_2006_poly2
 where stat_levl_ = 3
-group by id
 )
-select lvl0.nuts_id id0, lvl1.nuts_id id1, lvl2.nuts_id id2, lvl3.nuts_id id3
-from lvl0, lvl1, lvl2, lvl3
-where st_covers(lvl0.geom, lvl1.geom) and st_covers(lvl1.geom, lvl2.geom)
- and st_covers(lvl2.geom, lvl3.geom);
+select distinct
+lvl0.nuts_id id0,
+lvl1.nuts_id id1,
+lvl2.nuts_id id2,
+lvl3.nuts_id id3
+from lvl0
+left join lvl1 on st_covers(lvl0.geom, lvl1.geom)
+left join lvl2 on st_covers(lvl1.geom, lvl2.geom)
+left join lvl3 on st_covers(lvl2.geom, lvl3.geom);
+
+-- NUTS 2010
+create or replace view nuts_2010_levels as
+with
+lvl0 as (
+select nuts_id id0
+from nuts_2010_poly
+where stat_levl_ = 0
+),
+lvl1 as (
+select a.*, b.nuts_id id1
+from lvl0 a
+join nuts_2010_poly b on a.id0 = substring(b.nuts_id,1,2)
+where b.stat_levl_ = 1
+),
+lvl2 as (
+select a.*, b.nuts_id id2
+from lvl1 a
+join nuts_2010_poly b on a.id1 = substring(b.nuts_id,1,3)
+where b.stat_levl_ = 2
+)
+select a.*, b.nuts_id id3
+from lvl2 a
+join nuts_2010_poly b on a.id2 = substring(b.nuts_id,1,4)
+where b.stat_levl_ = 3
+order by a.id0, a.id1, a.id2, b.nuts_id;
---
+++ nuts_singlegeom.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE nuts_2010_poly2 AS                       --poly will be the new polygon table
+WITH dump AS (
+    SELECT id, stat_levl_, nuts_id,                       --columns from your multipolygon table 
+      (ST_DUMP(geom)).geom AS geom 
+    FROM nuts_2010_poly                             --the name of your multipolygon table
+) 
+SELECT id, stat_levl_, nuts_id, 
+  st_makevalid(geom::geometry(Polygon,4258)) geom         --type cast using SRID from multipolygon
+FROM dump;
+create index nuts_2010_poly2_nuts_id_idx on nuts_2010_poly2 using btree (nuts_id);
+create index nuts_2010_poly2_geom_idx on nuts_2010_poly2 using gist (geom);
+
+CREATE TABLE nuts_2010_poly3 AS                       --poly will be the new polygon table
+SELECT id, stat_levl_, nuts_id, 
+  st_makevalid(geom) geom         --type cast using SRID from multipolygon
+FROM nuts_2010_poly;
+create index nuts_2010_poly3_nuts_id_idx on nuts_2010_poly3 using btree (nuts_id);
+create index nuts_2010_poly3_geom_idx on nuts_2010_poly3 using gist (geom);
